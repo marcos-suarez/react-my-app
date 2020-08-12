6f9d0d45daf55f15698b27de2ee8706df414cfc3
August <marcos_augusto_suarez@yahoo.com.br>
2020-08-11 22:51:36 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react';
 import logo from './logo.svg'
-import './App.css'
+import './App.css';
 
-function App () {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello World! </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+class MyButton extends Component {
+  render() {
+    return (
+      <button
+        onClick={() => { this.props.handleClick(this.props.label); }}
+      >
+        {this.props.label}
+      </button>
+    );
+  }
 }
 
-export default App
+class MyLabel extends Component {
+  render() {
+    return <p>Clicked on: {this.props.text} </p>
+  }
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      labelText: '',
+    };
+  }
+
+  setLabelText = (labelText) => {
+    this.setState({ labelText });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <MyLabel text={this.state.labelText} />
+        <MyButton handleClick={this.setLabelText} label='Button 1' />
+        <MyButton handleClick={this.setLabelText} label='Button 2' />
+        <MyButton handleClick={this.setLabelText} label='Button 3' />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
