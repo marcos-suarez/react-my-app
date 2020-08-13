4105b1fcc3bbef8738f1b071da7a1f9541a16c8a
August <marcos_augusto_suarez@yahoo.com.br>
2020-08-13 15:03:51 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg'
-import './App.css';
+import './App.css'
 
 class MyButton extends Component {
-  render() {
+  render () {
     return (
       <button
-        onClick={() => { this.props.handleClick(this.props.label); }}
+        onClick={() => { this.props.handleClick(this.props.label) }}
       >
         {this.props.label}
       </button>
-    );
+    )
   }
 }
 
 class MyLabel extends Component {
-  render() {
+  render () {
     return <p>Clicked on: {this.props.text} </p>
   }
 }
 
-
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
-      labelText: '',
-    };
+      labelText: ''
+    }
+
+    this.setLabelText = this.setLabelText.bind(this)
   }
 
-  setLabelText = (labelText) => {
-    this.setState({ labelText });
+  setLabelText (labelText) {
+    this.setState({ labelText })
   }
 
-  render() {
+  render () {
     return (
-      <div className="App">
+      <div className='App'>
         <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <MyLabel text={this.state.labelText} />
-        <MyButton handleClick={this.setLabelText} label='Button 1' />
-        <MyButton handleClick={this.setLabelText} label='Button 2' />
-        <MyButton handleClick={this.setLabelText} label='Button 3' />
+          <img src={logo} className='App-logo' alt='logo' />
+          <MyLabel text={this.state.labelText} />
+          <MyButton handleClick={this.setLabelText} label='Button 1' />
+          <MyButton handleClick={this.setLabelText} label='Button 2' />
+          <MyButton handleClick={this.setLabelText} label='Button 3' />
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
